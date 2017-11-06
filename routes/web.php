ade0ff02390ed15f6a05a29579267de7e2a1ad52
George John <george@renfos.com>
2017-11-06 21:38:46 +0530

    Removed unused routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,5 @@
 // Resource routes  for block
 Route::group(['prefix' => set_route_guard('web') . '/block'], function () {
     Route::resource('block', 'BlockResourceController');
-});
-
-// Public  routes for block
-Route::get('blocks/', 'BlockPublicController@index');
-Route::get('blocks/{slug?}', 'BlockPublicController@show');
-
-// Resource routes  for category
-Route::group(['prefix' => set_route_guard('web') . '/block'], function () {
     Route::resource('category', 'CategoryResourceController');
 });
