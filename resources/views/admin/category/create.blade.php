e263756dc89e5ccf30898265d8203d753ba7faed
George John <george@renfos.com>
2017-11-30 22:01:42 +0530

    Update create blade

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -4,12 +4,12 @@
     <ul class="nav nav-tabs primary">
         <li class="active"><a href="#details" data-toggle="tab">Category</a></li>
         <div class="box-tools pull-right">
-            <button type="button" class="btn btn-primary btn-sm" data-action='CREATE' data-form='#blog-category-create'  data-load-to='#blog-category-entry' data-datatable='#blog-category-list'><i class="fa fa-floppy-o"></i> Save</button>
-            <button type="button" class="btn btn-default btn-sm" data-action='CLOSE' data-load-to='#blog-category-entry' data-href='{{guard_url('block/category/0')}}'><i class="fa fa-times-circle"></i> {{ trans('app.close') }}</button>
+            <button type="button" class="btn btn-primary btn-sm" data-action='CREATE' data-form='#block-category-create'  data-load-to='#block-category-entry' data-datatable='#block-category-list'><i class="fa fa-floppy-o"></i> Save</button>
+            <button type="button" class="btn btn-default btn-sm" data-action='CLOSE' data-load-to='#block-category-entry' data-href='{{guard_url('block/category/0')}}'><i class="fa fa-times-circle"></i> {{ trans('app.close') }}</button>
         </div>
     </ul>
     {!!Form::vertical_open()
-    ->id('blog-category-create')
+    ->id('block-category-create')
     ->method('POST')
     ->files('true')
     ->action(guard_url('block/category'))!!}
